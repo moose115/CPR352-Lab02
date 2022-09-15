758762be5cecfeb6270b23cfe0d8f973590ef0e2
Mustafa <mustafaucuncu@onet.pl>
2022-09-15 13:54:42 -0600

    Added a number Added code for a number and printed the number

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int number = 0;
+        System.out.println(number); 
     }
     
 }
